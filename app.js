ae842c873f9e86f7608ebcc5e48fdff7204f2909
Dude <arunshekhar001@gmail.com>
2020-10-26 10:12:03 +0530

    about-update

--- app.js
+++ app.js
@@ -107,6 +107,6 @@ app.delete("/mypost/:id", postDeleteArticle);
 app.put("/mypost/edit/:id", putMyPostEdit);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
